898ad1340a99f0aae447fb4a2bdec612ad92e08d
codeni80 <codeni80@gmail.com>
2019-09-20 16:08:02 -0400

    I /think/ I have saved sorting... sorted. Will need more testing after implementation of logged in users to confirm proper functionality.

--- ticketing/views.py
+++ ticketing/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 from ticketing.models import Technician, Ticket, Customer, Category, Status, TicketTable
 from django.views.generic import ListView
 from django_tables2 import RequestConfig
+import sys
 
 def ticketing_index(request):
-    queryset = Ticket.objects.all().order_by('-pk')
+    num = request.session.get('sort')
+    if not num:
+        num = '-pk'
+    try:
+        request.session['sort'] = request.GET['sort']
+        num = request.session['sort']
+    except:
+        pass
+
+    print(request.session.get('sort'), file=sys.stderr)
+    sort_by = request.session.get('sort')
+
+    queryset = Ticket.objects.all().order_by(sort_by)
     table = TicketTable(queryset)
     RequestConfig(request, paginate={"per_page": 20}).configure(table)
 
