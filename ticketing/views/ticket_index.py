38d19ed8494e2c681295a3a0ae0b4124196c2997
codeni80 <codeni80@gmail.com>
2019-10-09 16:41:13 -0400

    Filtering by Category is at least mostly functional. You have to assign u_filer_type manually for now, however by passing ?filter=<Category> to the homepage of the site (ticketing_index.html)

--- ticketing/views/ticket_index.py
+++ ticketing/views/ticket_index.py
@@ -15,6 +15,12 @@ from helpdesk.settings import STATICFILES_DIRS
 
 @login_required
 def ticketing_index(request):
+    categories = []
+    statuses = []
+    for stat in Status.objects.all():
+        statuses.append(stat.name)
+    for cat in Category.objects.all():
+        categories.append(cat.name)
     current_user = request.user
     user = get_user_model()
     print("STATIC DIRS: {}".format(STATICFILES_DIRS), file=sys.stderr)
@@ -26,6 +32,10 @@ def ticketing_index(request):
     try:
         # Try to set the sort type to what is requested by user
         current_user.u_sort_type = request.GET["sort"]
+        if request.GET["filter"] == 'None':
+            current_user.u_filter_type = None
+        else:
+            current_user.u_filter_type = request.GET["filter"]
         current_user.save()
         print("WE SAVED THE SORT TYPE", file=sys.stderr)
     except:
@@ -34,9 +44,69 @@ def ticketing_index(request):
     sort_by = current_user.u_sort_type
     print("Sorting by: {0}".format(sort_by), file=sys.stderr)
 
-    # Checking to see if logged in user is a technician (u_permission_level 2),
-    # or if they are a user (u_permission_level 1)
-    if current_user.u_permission_level == "2":
+    if current_user.u_filter_type is None:
+        queryset = all_ticket_helper(current_user, current_user.u_permission_level, sort_by, None)    
+    elif current_user.u_filter_type in categories:
+        queryset = category_helper(current_user, current_user.u_permission_level, sort_by, current_user.u_filter_type)
+    elif current_user.u_filter_type in statuses:
+        queryset = status_helper(current_user, current_user.u_permission_level, sort_by, current_user.u_filter_type)
+    elif current_user.u_filter_type == 'assigned':
+        queryset = assigned_ticket_helper(current_user, current_user.u_permission_level, current_sort_by, user.u_filter_type)
+    elif current_user.u_filter_type == 'active':
+        queryset = active_tickets_helper(current_user, current_user.u_permission_level, current_sort_by, user.u_filter_type)
+    elif current_user.u_filter_type == 'closed':
+        queryset = closed_tickets_helper(current_user, current_user.u_permission_level, current_sort_by, user.u_filter_type)
+    elif current_user.u_filter_type == 'unasigned':
+        queryset = unasigned_tickets_helper(current_user, current_user.u_permission_level, current_sort_by, user.u_filter_type)
+    else:
+        queryset = Ticket.objects.filter(c_info=None)
+    table = TicketTable(queryset)
+    RequestConfig(request).configure(table)
+    return render(request, "ticketing_index.html", {"table": table})
+
+def category_helper(current_user, perms, sort_by, filter_by):
+    if perms == "2":
+        print("FILTER: {}".format(filter_by), file=sys.stderr)
+        if sort_by == 't_subject' or sort_by == '-t_subject':
+            if sort_by[0] == "-":
+                sort_by = sort_by[1:]
+                queryset = Ticket.objects.filter(
+                    t_category__name__contains=filter_by).annotate(
+                    t_subject_lower=Lower("t_subject")
+                ).order_by(Lower(sort_by).desc())
+            else:
+                queryset = Ticket.objects.filter(
+                    t_category__name__contains=filter_by).annotate(
+                    t_subject_lower=Lower("t_subject")
+                ).order_by(Lower(sort_by).asc())
+        else:
+            queryset = Ticket.objects.filter(t_category__name__contains=filter_by).order_by(sort_by)
+    else:
+        # Non-technicians only see tickets made by/for them.
+        if sort_by == 't_subject' or sort_by == '-t_subject':
+            if sort_by[0] == "-":
+                sort_by = sort_by[1:]
+                queryset = Ticket.objects.filter(
+                    c_info__username=current_user.username, 
+                    t_category__name__contains=filter_by
+                ).annotate(t_subject_lower=Lower("t_subject")).order_by(Lower(sort_by).desc())
+            else:
+                queryset = Ticket.objects.filter(
+                    c_info__username=current_user.username,
+                    t_category__name__contains=filter_by
+                ).annotate(t_subject_lower=Lower("t_subject")).order_by(Lower(sort_by).asc())
+        else:
+            queryset = Ticket.objects.filter(
+                c_info__username=current_user.username,
+                t_category__name__contains=filter_by
+            ).order_by(sort_by)
+    return queryset
+
+def status_helper(current_user, perms, sort_by, filter_by):
+    pass
+
+def all_ticket_helper(current_user, perms, sort_by, filter_by):
+    if perms == "2":
         if sort_by == 't_subject' or sort_by == '-t_subject':
             if sort_by[0] == "-":
                 sort_by = sort_by[1:]
@@ -49,11 +119,6 @@ def ticketing_index(request):
                 ).order_by(Lower(sort_by).asc())
         else:
             queryset = Ticket.objects.all().order_by(sort_by)
-
-        table = TicketTable(queryset)
-        RequestConfig(request).configure(table)
-
-        return render(request, "ticketing_index.html", {"table": table})
     else:
         # Non-technicians only see tickets made by/for them.
         if sort_by == 't_subject' or sort_by == '-t_subject':
@@ -70,29 +135,17 @@ def ticketing_index(request):
             queryset = Ticket.objects.filter(
                 c_info__username=current_user.username
             ).order_by(sort_by)
+    return queryset
 
-        table = TicketTable(queryset)
-        RequestConfig(request, paginate={"per_page": 20}).configure(table)
 
-        return render(request, "ticketing_index.html", {"table": table})
-
-def category_helper(sort_by, filter_by):
+def assigned_ticket_helper(current_user, perms, sort_by, filter_by):
     pass
 
-def status_helper(sort_by, filter_by):
+def active_tickets_helper(current_user, perms, sort_by, filter_by):
     pass
 
-def all_ticket_helper(sort_by, filter_by):
+def closed_tickets_helper(current_user, perms, sort_by, filter_by):
     pass
 
-def assigned_ticket_helper(sort_by, filter_by):
-    pass
-
-def active_tickets_helper(sort_by, filter_by):
-    pass
-
-def closed_tickets_helper(sort_by, filter_by):
-    pass
-
-def unasigned_tickets_helper(sort_by, filter_by):
+def unasigned_tickets_helper(current_user, perms, sort_by, filter_by):
     pass
